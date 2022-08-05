0a7fc6b44a2f2665c75d185e481fd40c6961b3e9
Lucas Zadrozny <lucas.zadrozny@hotmail.com>
2022-08-06 01:35:40 +1000

    Added Mariner9Camera - derived from Mariner10Camera. Added mar92isis - derived from mar102isis. Doesn't support importing of ISIS2 cubes because I don't have a reference Mariner 9 cube! Adding associated data.

---
+++ isis/src/mariner/objs/Mariner9Camera/Mariner9Camera.h
@@ -0,0 +1,83 @@
+#ifndef Mariner9Camera_h
+#define Mariner9Camera_h
+
+/** This is free and unencumbered software released into the public domain.
+
+The authors of ISIS do not claim copyright on the contents of this file.
+For more details about the LICENSE terms and the AUTHORS, you will
+find files of those names at the top level of this repository. **/
+
+/* SPDX-License-Identifier: CC0-1.0 */
+
+#include "FramingCamera.h"
+
+namespace Isis {
+  /**
+   * @brief Mariner9 Camera Model
+   *
+   * This is the camera model for both mariner9, both cameras A (wide angle)
+   * and B (narrow angle).
+   *
+   * @ingroup SpiceInstrumentsAndCameras
+   * @ingroup Mariner9
+   *
+   * @author 2009-04-13 Tracie Sucharski
+   *
+   * @internal
+   *   @history 2009-12-18 Tracie Sucharski - Changed inheritance from Camera to
+   *                          FramingCamera.
+   *   @history 2010-02-22 Tracie Sucharski - Preface naif includes with /naif.
+   *   @history 2010-03-04 Tracie Sucharski - Removed couts.
+   *   @history 2010-03-04 Tracie Sucharski - Added throw when creation of
+   *                          ReseauDistortion fails.
+   *   @history 2011-01-14 Travis Addair - Added new CK/SPK accessor methods,
+   *                          pure virtual in Camera, implemented in mission
+   *                          specific cameras.
+   *   @history 2011-02-09 Steven Lambright - Major changes to camera classes.
+   *   @history 2011-05-03 Jeannie Walldren - Added ShutterOpenCloseTimes()
+   *                          method. Updated unitTest to test for new methods.
+   *                          Updated documentation. Added Isis Disclaimer to
+   *                          files. Added NAIF error check to constructor.
+   *   @history 2012-07-06 Debbie A. Cook, Updated Spice members to be more compliant with Isis
+   *                          coding standards. References #972.
+   *   @history 2015-08-25 Ian Humphrey and Makayla Shepherd - Added new data members and methods
+   *                           to get spacecraft and instrument names. Extended unit test to test
+   *                           these methods.
+   *   @history 2015-10-16 Ian Humphrey - Removed declarations of spacecraft and instrument
+   *                           members and methods and removed implementation of these methods
+   *                           since Camera now handles this. References #2335.
+   */
+  class Mariner9Camera : public FramingCamera {
+    public:
+      Mariner9Camera(Cube &cube);
+      //! Destroys the Mariner9Camera Object
+      ~Mariner9Camera() {};
+      virtual std::pair <iTime, iTime> ShutterOpenCloseTimes(double time,
+                                                             double exposureDuration);
+
+      /**
+       * CK frame ID -  - Instrument Code from spacit run on CK
+       *
+       * @return @b int The appropriate instrument code for the "Camera-matrix"
+       *         Kernel Frame ID
+       */
+      virtual int CkFrameId() const { return (-76000); }
+
+      /**
+       * CK Reference ID - J2000
+       *
+       * @return @b int The appropriate instrument code for the "Camera-matrix"
+       *         Kernel Reference ID
+       */
+      virtual int CkReferenceId() const { return (1); }
+
+      /**
+       *  SPK Reference ID - J2000
+       *
+       * @return @b int The appropriate instrument code for the Spacecraft
+       *         Kernel Reference ID
+       */
+      virtual int SpkReferenceId() const { return (1); }
+  };
+};
+#endif
---
+++ isis/src/mariner/objs/Mariner9Camera/unitTest.cpp
@@ -0,0 +1,158 @@
+/** This is free and unencumbered software released into the public domain.
+
+The authors of ISIS do not claim copyright on the contents of this file.
+For more details about the LICENSE terms and the AUTHORS, you will
+find files of those names at the top level of this repository. **/
+
+/* SPDX-License-Identifier: CC0-1.0 */
+
+#include <iomanip>
+#include <iostream>
+
+#include "Camera.h"
+#include "CameraFactory.h"
+#include "FileName.h"
+#include "IException.h"
+#include "iTime.h"
+#include "Mariner9Camera.h"
+#include "Preference.h"
+#include "Pvl.h"
+#include "PvlGroup.h"
+
+using namespace std;
+using namespace Isis;
+
+void TestLineSamp(Camera *cam, double samp, double line);
+
+int main(void) {
+  Preference::Preferences(true);
+
+  cout << "Unit Test for Mariner9Camera..." << endl;
+  try {
+    // These should be lat/lon at center of image. To obtain these numbers for a new cube/camera,
+    // set both the known lat and known lon to zero and copy the unit test output "Latitude off by: "
+    // and "Longitude off by: " values directly into these variables.
+    char files[2][1024] = { "$ISISTESTDATA/isis/src/mariner/unitTestData/27265.cub",
+                            "$ISISTESTDATA/isis/src/mariner/unitTestData/166474.cub"
+                          };
+
+    double knownLat[2] = { -21.1110851813477538,
+                           -22.58558961173848
+                         };
+    double knownLon[2] = { 2.9545840388299451,
+                           292.0697686733246
+                         };
+
+    vector< pair <int, int> > corners;
+
+    //  27265.cub  Mariner A
+    corners.push_back(std::make_pair(14, 1));
+    corners.push_back(std::make_pair(831, 1));
+    corners.push_back(std::make_pair(9, 700));
+    corners.push_back(std::make_pair(829, 700));
+    //  166474.cub Mariner B
+    corners.push_back(std::make_pair(36, 1));
+    corners.push_back(std::make_pair(829, 1));
+    corners.push_back(std::make_pair(55, 700));
+    corners.push_back(std::make_pair(830, 700));
+
+
+    for(unsigned int i = 0; i < sizeof(knownLat) / sizeof(double); i++) {
+      Cube c(files[i], "r");
+      Mariner9Camera *cam = (Mariner9Camera *) CameraFactory::Create(c);
+      cout << "FileName: " << FileName(c.fileName()).name() << endl;
+      cout << "CK Frame: " << cam->instrumentRotation()->Frame() << endl << endl;
+      cout.setf(std::ios::fixed);
+      cout << setprecision(9);
+
+      // Test kernel IDs
+      cout << "Kernel IDs: " << endl;
+      cout << "CK Frame ID = " << cam->CkFrameId() << endl;
+      cout << "CK Reference ID = " << cam->CkReferenceId() << endl;
+      cout << "SPK Target ID = " << cam->SpkTargetId() << endl;
+      cout << "SPK Reference ID = " << cam->SpkReferenceId() << endl << endl;
+
+      // Test name methods
+      cout << "Spacecraft Name Long: " << cam->spacecraftNameLong() << endl;
+      cout << "Spacecraft Name Short: " << cam->spacecraftNameShort() << endl;
+      cout << "Instrument Name Long: " << cam->instrumentNameLong() << endl;
+      cout << "Instrument Name Short: " << cam->instrumentNameShort() << endl << endl;
+
+      // Test Shutter Open/Close
+      const PvlGroup &inst = c.label()->findGroup("Instrument", Pvl::Traverse);
+      double exposureDuration = ((double) inst["ExposureDuration"])/1000;
+      QString stime = inst["StartTime"];
+      double et; // StartTime keyword is the center exposure time
+      str2et_c(stime.toLatin1().data(), &et);
+      pair <iTime, iTime> shuttertimes = cam->ShutterOpenCloseTimes(et, exposureDuration);
+      cout << "Shutter open = " << shuttertimes.first.Et() << endl;
+      cout << "Shutter close = " << shuttertimes.second.Et() << endl << endl;
+
+      // Test all four corners to make sure the conversions are right
+      cout << "For upper left corner ..." << endl;
+      TestLineSamp(cam, corners[i*4].first, corners[i*4].second);
+
+      cout << "For upper right corner ..." << endl;
+      TestLineSamp(cam, corners[i*4+1].first, corners[i*4+1].second);
+
+      cout << "For lower left corner ..." << endl;
+      TestLineSamp(cam, corners[i*4+2].first, corners[i*4+2].second);
+
+      cout << "For lower right corner ..." << endl;
+      TestLineSamp(cam, corners[i*4+3].first, corners[i*4+3].second);
+
+      double samp = cam->Samples() / 2;
+      double line = cam->Lines() / 2;
+      cout << "For center pixel position ..." << endl;
+
+      if(!cam->SetImage(samp, line)) {
+        cout << "ERROR" << endl;
+        return 0;
+      }
+
+      if(abs(cam->UniversalLatitude() - knownLat[i]) < 1E-10) {
+        cout << "Latitude OK" << endl;
+      }
+      else {
+        cout << setprecision(16) << "Latitude off by: " << cam->UniversalLatitude() - knownLat[i] << endl;
+      }
+
+      if(abs(cam->UniversalLongitude() - knownLon[i]) < 2E-10) {
+        cout << "Longitude OK" << endl;
+      }
+      else {
+        cout << setprecision(16) << "Longitude off by: " << cam->UniversalLongitude() - knownLon[i] << endl;
+      }
+      cout << endl << "--------------------------------------------" << endl;
+    }
+
+    // Test exception: camera is not a supported Kaguya camera
+    cout << endl << "Testing exceptions:" << endl << endl;
+    Cube test("$ISISTESTDATA/isis/src/hayabusa/unitTestData/st_2530292409_v.cub", "r");
+    Mariner9Camera mCam(test);
+  }
+  catch(IException &e) {
+    e.print();
+  }
+}
+
+void TestLineSamp(Camera *cam, double samp, double line) {
+  bool success = cam->SetImage(samp, line);
+
+  if (success) {
+    success = cam->SetUniversalGround(cam->UniversalLatitude(), cam->UniversalLongitude());
+  }
+
+  if(success) {
+    double deltaSamp = samp - cam->Sample();
+    double deltaLine = line - cam->Line();
+    if(fabs(deltaSamp) < 0.001) deltaSamp = 0;
+    if(fabs(deltaLine) < 0.001) deltaLine = 0;
+    cout << "DeltaSample = " << deltaSamp << endl;
+    cout << "DeltaLine = " << deltaLine << endl << endl;
+  }
+  else {
+    cout << "DeltaSample = ERROR" << endl;
+    cout << "DeltaLine = ERROR" << endl << endl;
+  }
+}
