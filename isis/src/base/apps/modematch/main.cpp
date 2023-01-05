eb9f2f9797c2722dc181563c3093afeef7fdef52
Lucas Zadrozny <lucas.zadrozny@hotmail.com>
2023-01-05 12:33:43 +1100

    modematch: Scales the mode of FROM to equal MATCH

---
+++ isis/src/base/apps/modematch/main.cpp
@@ -0,0 +1,49 @@
+#include "Isis.h"
+#include "Cube.h"
+#include "Histogram.h"
+#include "ProcessByLine.h"
+#include "UserInterface.h"
+#include "IException.h"
+
+#include <memory>
+
+using namespace Isis;
+
+std::vector<double> bandScales;
+
+static void modematch(std::vector<Buffer *> &in, std::vector<Buffer *> &out);
+
+void IsisMain() {
+  UserInterface &ui = Application::GetUserInterface();
+  Cube from, match;
+
+  from.open(ui.GetCubeName("FROM"));
+  match.open(ui.GetCubeName("MATCH"));
+
+  for (int i = 1; i <= from.bandCount(); ++i) {
+    std::unique_ptr<Histogram> fromStats(from.histogram(i));
+    std::unique_ptr<Histogram> matchStats(match.histogram(i));
+
+    bandScales.emplace_back(fromStats->Mode() / matchStats->Mode());
+  }
+
+  ProcessByLine p;
+  p.AddInputCube(&from, false);
+  p.SetOutputCube("TO");
+
+  p.ProcessCubes(modematch, false);
+}
+
+static void modematch(std::vector<Buffer *> &in, std::vector<Buffer *> &out) {
+  const auto& from = *in[0];
+  auto& to = *out[0];
+
+  for (int i = 0; i < from.SampleDimension(); ++i) {
+    if (IsSpecial(from[i])) {
+      to[i] = from[i];
+    }
+    else {
+      to[i] = from[i] / bandScales[from.Band() - 1];
+    }
+  }
+}
